224fb0776d7d89450b543c518fb80d73a8791b48
Benjamin R. Bray <benrbray@gmail.com>
2023-09-12 12:34:14 +0200

    lezer metavariables

--- web/src/lezer/lang.ts
+++ web/src/lezer/lang.ts
@@ -2,46 +2,47 @@
 import { parser } from "./lang.grammar"
 // import { parser } from "./lezer/binop.grammar"
 import { foldNodeProp, foldInside, indentNodeProp } from "@codemirror/language"
-import { styleTags, tags as t, Tag as T } from "@lezer/highlight"
-import {tags} from "@lezer/highlight"
+import { styleTags, tags, Tag as T } from "@lezer/highlight"
 
 import { LRLanguage, LanguageSupport, syntaxHighlighting, HighlightStyle } from "@codemirror/language"
 
 ////////////////////////////////////////////////////////////
 
-const tagMetaVar = T.define(t.variableName);
-const tagTypeVar = T.define(t.variableName);
-const tagTermVar = T.define(t.variableName);
+const tagMetaVar = T.define(tags.meta);
+const tagTypeVar = T.define(tags.typeName);
+const tagTermVar = T.define(tags.variableName);
 
 let parserWithMetadata = parser.configure({
   props: [
     styleTags({
-      Identifier: t.variableName,
+      Identifier: tags.variableName,
       /* literals */
-      BooleanLiteral: t.bool,
-      StringLiteral: t.string,
-      IntLiteral: t.integer,
+      BooleanLiteral: tags.bool,
+      StringLiteral: tags.string,
+      IntLiteral: tags.integer,
       /* commends */
-      LineComment: t.lineComment,
+      LineComment: tags.lineComment,
       /* operators */
-      ArithOp: t.arithmeticOperator,
-      CmpOp: t.compareOperator,
+      ArithOp: tags.arithmeticOperator,
+      CmpOp: tags.compareOperator,
       /* keywords */
-      "forall" : t.keyword,
-      "." : t.keyword,
-      "let": t.keyword,
-      "in": t.keyword,
-      "if": t.keyword,
-      "then": t.keyword,
-      "else": t.keyword,
-      "=": t.keyword,
-      "( )": t.paren,
-      Symbol: t.punctuation,
+      "forall" : tags.keyword,
+      "." : tags.keyword,
+      "let": tags.keyword,
+      "in": tags.keyword,
+      "if": tags.keyword,
+      "then": tags.keyword,
+      "else": tags.keyword,
+      "=": tags.keyword,
+      "( )": tags.paren,
+      Symbol: tags.punctuation,
       /* definitions */
-      Lambda: t.definitionKeyword,
-      LambdaArrow: t.definitionKeyword,
+      Lambda: tags.definitionKeyword,
+      LambdaArrow: tags.definitionKeyword,
       /* types */
-      TypeCon: t.typeName,
+      MetaVar: tagMetaVar,
+      TypeVar: tagTypeVar,
+      TypeCon: tags.typeName,
     }),
     indentNodeProp.add({
       // TODO (Ben @ 2023/09/12) handle indentatoin
@@ -84,6 +85,8 @@ const highlightStyle = HighlightStyle.define([
   {tag: tags.compareOperator, color: colorPrimary },
   /* types */
   {tag: tags.typeName, color: "#a85800" },
+  {tag: tagTypeVar },
+  {tag: tagMetaVar, color: "#ff00d6" },
 ]);
 
 export const langHighlight = syntaxHighlighting(highlightStyle);
